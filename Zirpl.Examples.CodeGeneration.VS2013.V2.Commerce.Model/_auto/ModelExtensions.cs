73365c9a39bc49245e1445d2015265ba73384327
nlafratta <nlafratta@zirpl.com>
2014-12-06 15:04:42 -0500

    oops

---
+++ Zirpl.Examples.CodeGeneration.VS2013.V2.Commerce.Model/_auto/ModelExtensions.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Zirpl.AppEngine.Model;
+using Zirpl.AppEngine.Model.Extensibility;
+using Zirpl.Collections;
+
+namespace Zirpl.Examples.Commerce.Model
+{
+    public static partial class ModelExtensions
+    {
+        public static void Save(this AddressBase entity)
+        {
+        }
+        public static void Save(this DomesticAddress entity)
+        {
+        }
+        public static void Save(this InternationalAddress entity)
+        {
+        }
+        public static void Save(this AddressBaseExtendedFieldValue entity)
+        {
+        }
+        public static void Delete(this AddressBase entity)
+        {
+        }
+        public static void Delete(this AddressBaseExtendedFieldValue entity)
+        {
+        }
+    }
+}
---
+++ Zirpl.Examples.CodeGeneration.VS2013.V2.Commerce.Model/_auto/NamePrefixTypeEnum.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Zirpl.AppEngine.Model;
+using Zirpl.AppEngine.Model.Extensibility;
+using Zirpl.Collections;
+
+namespace Zirpl.Examples.Commerce.Model
+{
+    public enum NamePrefixTypeEnum : int
+    {
+        Dr = 1,
+        Mr = 2,
+    }
+}
