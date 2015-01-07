70e6ef9d9d1ea18f7dd240d81d92ea6fe293307a
Nathan LaFratta <nlafratta@zirpl.com>
2015-01-07 05:50:33 -0500

    combing the AssemblyInfo.cs files, moved AppEngine code from Common to AppEngine

--- Zirpl.AppEngine.VisualStudioAutomation/Properties/AssemblyInfo.cs
+++ Zirpl.AppEngine.VisualStudioAutomation/Properties/AssemblyInfo.cs
@@ -2,25 +2,28 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Zirpl.AppEngine.CodeGeneration")]
+[assembly: AssemblyTitle("Zirpl.AppEngine.VisualStudioAutomation")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Zirpl.AppEngine.CodeGeneration")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("Zirpl.AppEngine.VisualStudioAutomation")]
+[assembly: AssemblyCompany("Zirpl Software")]
+[assembly: AssemblyCopyright("Copyright © 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+#if !PORTABLE
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("72dea220-141f-46e6-8653-6558a8529e0e")]
+//[assembly: Guid("")]
+#endif
 
 // Version information for an assembly consists of the following four values:
 //
@@ -33,5 +36,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0")]
+
 [assembly: InternalsVisibleTo("Zirpl.AppEngine.VisualStudioAutomation.Tests")]
